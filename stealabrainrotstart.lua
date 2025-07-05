cac270d3932cfe897c038dba3c8a2142c79d0b3b
Adams <97788419+Adamits1@users.noreply.github.com>
2025-07-05 15:38:05 +0300

    Update stealabrainrotstart.lua

--- stealabrainrotstart.lua
+++ stealabrainrotstart.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Adamits1/adamchit123/main/stealabrainrotbypass.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Adamits1/adamchit123/main/stealabrainrot.lua"))()
